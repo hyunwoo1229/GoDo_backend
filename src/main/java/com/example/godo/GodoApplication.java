1810b022897c3a352eca4f0baf56a7e6f3fcf04c
조현우 <hynoo20011229@gmail.com>
2026-04-21 17:57:02 +0900

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/example/godo/GodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.godo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/godo/GodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.godo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
