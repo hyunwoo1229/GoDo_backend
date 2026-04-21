68216849b1edaf005db0143ed18225bc49d2e295
조현우 <hynoo20011229@gmail.com>
2026-04-21 21:22:49 +0900

    Object Storage 연동

--- src/main/java/com/example/godo/GodoApplication.java
+++ src/main/java/com/example/godo/GodoApplication.java
@@ -2,8 +2,10 @@ package com.example.godo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class GodoApplication {
 
 	public static void main(String[] args) {
